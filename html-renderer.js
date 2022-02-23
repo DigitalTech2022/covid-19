1432f3511c17286dffcded25150c03f176d81967
DigitalTech2022 <100289693+DigitalTech2022@users.noreply.github.com>
2022-02-24 00:23:24 +0800

    Update html-renderer.js

--- html-renderer.js
+++ html-renderer.js
@@ -72,15 +72,17 @@ onValue(compulsory_text, (snapshot) => {
                     html += "</div>";
                 html += "</div>";
             html += "</div>";
-            html += "<div data-hover='false' data-delay='0' data-w-id='491306b6-95d0-03c3-f777-3d6aa11c2282' style='height: 45px;' class='ct-content-second-row-dropdown w-dropdown'>";
-                html += "<div class='second-row-accordion-toggle w-dropdown-toggle' id='w-dropdown-toggle-"+webflow_dropdown_count+"' aria-controls='w-dropdown-list-"+webflow_dropdown_count+"' aria-haspopup='menu' aria-expanded='false' role='button' tabindex='0'>";
+
+
+            html += "<div data-hover='false' data-delay='0' data-w-id='d3b00914-fb7d-6ee0-9890-ce02db0724a9' style='height:45px' class='ct-content-second-row-dropdown w-dropdown'>";
+                html += "<div class='second-row-accordion-toggle w-dropdown-toggle'>";
                     html += "<div class='specific-date-header-div'>";
                         html += "<h6 class='ct-content-specific-date-heading'>指明期間及時段</h6>";
                         html += "<h6 class='ct-content-specific-date-description'>(視情況而定任何一個 期間及時段)</h6>";
                     html += "</div>";
-                    html += "<div data-w-id='dcdd92ba-d953-a645-c063-ce67de900232' style='transform: translate3d(0px, 0px, 0px) scale3d(1, 1, 1) rotateX(0deg) rotateY(0deg) rotateZ(0deg) skew(0deg, 0deg); transform-style: preserve-3d;' class='dropdown-arrow'></div>";
+                    html += "<div data-w-id='d3b00914-fb7d-6ee0-9890-ce02db0724b4' style='-webkit-transform:translate3d(0, 0, 0) scale3d(1, 1, 1) rotateX(0) rotateY(0) rotateZ(0deg) skew(0, 0);-moz-transform:translate3d(0, 0, 0) scale3d(1, 1, 1) rotateX(0) rotateY(0) rotateZ(0deg) skew(0, 0);-ms-transform:translate3d(0, 0, 0) scale3d(1, 1, 1) rotateX(0) rotateY(0) rotateZ(0deg) skew(0, 0);transform:translate3d(0, 0, 0) scale3d(1, 1, 1) rotateX(0) rotateY(0) rotateZ(0deg) skew(0, 0)' class='dropdown-arrow'></div>";
                 html += "</div>";
-                html += "<nav class='dropdown-list w-dropdown-list' id='w-dropdown-list-"+webflow_dropdown_count+"' aria-labelledby='w-dropdown-toggle-"+webflow_dropdown_count+"'>";
+                html += "<nav class='dropdown-list w-dropdown-list'>";
                     html += "<div class='specific-date-wrapper'>";
                         html += "<div class='specific-date-div'>";
                             html += "<div class='specific-date-heading-div'>";
@@ -93,6 +95,8 @@ onValue(compulsory_text, (snapshot) => {
                     html += "</div>";
                 html += "</nav>";
             html += "</div>";
+
+
             html += "<div class='ct-content-separated-line'></div>";
             html += "<div class='ct-content-third-row'>";
                 html += "<h4 class='ct-content-testing-dates-heading'>須進行檢測日期</h4>";
@@ -147,6 +151,8 @@ onValue(compulsory_text, (snapshot) => {
                     html += "</div>";
                 html += "</div>";
             html += "</div>";
+
+
             webflow_dropdown_count += 1;
             html += "<div data-hover='false' data-w-id='3f3dea53-7180-03b5-4c3e-c9b4f0db25bd' data-delay='0' style='height: 24px;' class='ct-content-third-row-dropdown w-dropdown'>";
                 html += "<div class='third-row-accordion-toggle w-dropdown-toggle' id='w-dropdown-toggle-"+webflow_dropdown_count+"' aria-controls='w-dropdown-list-"+  webflow_dropdown_count +"' aria-haspopup='menu' aria-expanded='false' role='button' tabindex='0'>";
@@ -205,6 +211,8 @@ onValue(compulsory_text, (snapshot) => {
                     html += "</div>";
                 html += "</nav>";
             html += "</div>";
+
+
         html += "</div>";
         document.getElementById("ct-custom-flexbox").innerHTML += html;
         webflow_dropdown_count += 1;
